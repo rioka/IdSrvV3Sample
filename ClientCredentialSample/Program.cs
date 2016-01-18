17fe8047b717518006d8440122fbff1be3071dc0
r.dozzo <r.dozzo@orwellg.com>
2016-01-18 15:31:11 +0100

    - factoring - New sample client for "Resource Owner Password" flow

--- ClientCredentialSample/Program.cs
+++ ClientCredentialSample/Program.cs
@@ -1,79 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using IdentityModel.Client;
+using IdSrvV3Sample.Common;
 
 namespace IdSrvV3Sample.ClientCredentialSample
 {
   class Program
   {
-    private static string _isBaseAddress = ConfigurationManager.AppSettings["Is:Url"];
-
     static void Main(string[] args)
     {
-      if (_isBaseAddress.Last() != '/')
-        _isBaseAddress += @"/";
+      var client = new ApiClient();
 
-      var client = new HttpClient() {
-        BaseAddress = new Uri(ConfigurationManager.AppSettings["Api:Url"])
-      };
-
-      AccessUnprotectedResource(client, "samples");
+      client.AccessUnprotectedResource("samples");
       
       // unauthorized!
-      AccessProtectedResource(client, "samples");
-      var token = GetToken();
+      client.AccessProtectedResource("samples");
+
       // authorized
-      AccessProtectedResource(client, "samples", token.AccessToken);
+      var token = new ISClient().GetClientCredentialToken();
+      client.AccessProtectedResource("samples", token.AccessToken);
 
-      client.Dispose();
-    }
-
-    private static void AccessUnprotectedResource(HttpClient client, string path)
-    {
-      Console.WriteLine("Requesting an unprotected resource...");
-      var result = client.GetStringAsync(path).Result;
-
-      Console.WriteLine("Result for an unprotected resource: {0}", result);
-    }
-
-    private static void AccessProtectedResource(HttpClient client, string path, string token = null)
-    {
-      Console.WriteLine("Requesting a protected resource...");
-      if (!string.IsNullOrWhiteSpace(token))
-      {
-        Console.WriteLine("Setting authorization token...");
-        client.SetBearerToken(token);
-      }
-      else
-      {
-        Console.WriteLine("... without an authorization token...");
-      }
-      
-      var body = new Dictionary<string, string>();
-      var result = client.PostAsync(path, new FormUrlEncodedContent(body)).Result;
-
-      Console.WriteLine(result);
-
-      if (result.IsSuccessStatusCode)
-      {
-        var content = result.Content.ReadAsStringAsync().Result;
-        Console.WriteLine("Result for a protected resource: {0}", content);
-      }
-    }
-
-    private static TokenResponse GetToken()
-    {
-      var client = new TokenClient(
-        _isBaseAddress + "connect/token",
-        ConfigurationManager.AppSettings["Is:Client:ClientId"],
-        ConfigurationManager.AppSettings["Is:Client:Secret"]);
-
-      Console.WriteLine("Requesting a token...");
-      return client.RequestClientCredentialsAsync("read write").Result;      
+      Console.ReadLine();
     }
   }
 }
--- IdSrv/Configuration/Clients.cs
+++ IdSrv/Configuration/Clients.cs
@@ -6,27 +6,61 @@ namespace IdSrvV3Sample.IdSrv.Configuration
 {
   public class Clients
   {
+    static readonly int TenMinutes = 360;
+    
     public static IEnumerable<Client> Get()
     {
       return new List<Client>() {
-        new Client {
-          ClientName = "Client Credentials Flow Client",
-          Enabled = true,
-          ClientId = "client",
-          ClientSecrets = new List<Secret> {
-            new Secret("secret".Sha256())
-          },
-          Flow = Flows.ClientCredentials,
-
-          AllowedScopes = new List<string> {
-            "read",
-            "write"
-          },
-
-          AccessTokenType = AccessTokenType.Jwt,
-          AccessTokenLifetime = 360,
-        }
+        GetAClientCredentialFlowClient(),
+        GetAResourceOwnerFlowClient()
       };
     }
+
+    #region Internals
+
+    private static Client GetAResourceOwnerFlowClient()
+    {
+      return new Client {
+        ClientName = "Resource Owner Flow Client",
+        Enabled = true,
+        ClientId = "roclient",
+        ClientSecrets = new List<Secret> {
+          new Secret("secret".Sha256())
+        },
+        Flow = Flows.ResourceOwner,
+
+        AllowedScopes = new List<string> {
+          "read",
+          "write"
+        },
+
+        AccessTokenType = AccessTokenType.Jwt,
+        AccessTokenLifetime = TenMinutes,
+      };
+
+    }
+
+    static Client GetAClientCredentialFlowClient()
+    {
+      return new Client {
+        ClientName = "Client Credentials Flow Client",
+        Enabled = true,
+        ClientId = "client",
+        ClientSecrets = new List<Secret> {
+          new Secret("secret".Sha256())
+        },
+        Flow = Flows.ClientCredentials,
+
+        AllowedScopes = new List<string> {
+          "read",
+          "write"
+        },
+
+        AccessTokenType = AccessTokenType.Jwt,
+        AccessTokenLifetime = TenMinutes,
+      };
+    } 
+
+    #endregion      
   }
 }
